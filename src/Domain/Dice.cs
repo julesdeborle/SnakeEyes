8c6e7c30e17cebcb5db3e96bbbc853837b26c0db
Jules De Borle <jules.deborle@student.hogent.be>
2021-10-22 15:09:45 +0200

    test

---
+++ src/Domain/Dice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Domain
+{
+    internal class Dice
+    {
+        public int Dots { get; set; }
+
+        private static Random _randomizer = new();
+
+        public Dice()
+        {
+            Dots = 6;
+        }
+
+        public void Roll()
+        {
+            Dots = _randomizer.Next(1, 7);
+        }
+    }
+}
---
+++ src/Domain/Game.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Game
+    {
+
+        public readonly List<int> _highscores = new ();
+        public int Eye1 => _dice1.Dots;
+        public int Eye2 => _dice2.Dots;
+
+        private Dice _dice1;
+        private Dice _dice2;
+
+        public bool HasSnakeEyes => _dice1.Dots == 1 && _dice2.Dots == 1;
+        public IReadOnlyList<int> HighScores => _highscores.AsReadOnly();
+        public int Total { get; private set; }
+        public Game()
+        {
+            Initialize();
+         
+        }
+
+        public void Initialize()
+        {
+            _dice1 = new();
+            _dice2 = new();
+        }
+
+        public void Play()
+        {
+            _dice1.Roll();
+            _dice2.Roll();
+
+            if (HasSnakeEyes)
+            {
+                _highscores.Add(Total);
+                Total = 0;
+            }
+            else
+            {
+                Total += Eye1 + Eye2;
+            }
+        }
+
+        public void Restart()
+        {
+            Initialize();
+        }
+    }
+}
